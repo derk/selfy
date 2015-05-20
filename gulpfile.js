45331e82b4e841cf49b9e54dd33a42e1e7d11ea4
Russian <russian.reboucas@gmail.com>
2015-05-20 20:44:29 -0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    stylus = require('gulp-stylus'),
-    jade = require('gulp-jade'),
-    concat = require('gulp-concat'),
-    flatten = require('gulp-flatten'),
-    shell = require('gulp-shell');
+var gulp        = require('gulp'),
+    uglify      = require('gulp-uglify'),
+    stylus      = require('gulp-stylus'),
+    jade        = require('gulp-jade'),
+    concat      = require('gulp-concat'),
+    flatten     = require('gulp-flatten'),
+    shell       = require('gulp-shell')
+    browserSync = require('browser-sync').create(),
+    reload      = browserSync.reload;
 
 gulp.task('concat', function() {
   gulp.src(['./src/*.js', './src/app/**/*.js', './src/shared/**/*.js'])
   .pipe(concat('application.js'))
-  .pipe(gulp.dest('./www/'));
+  .pipe(gulp.dest('./www/'))
+  .pipe(reload({stream:true}));
 });
 
 gulp.task('jade', function() {
@@ -19,23 +22,42 @@ gulp.task('jade', function() {
   gulp.src(['./src/app/components/**/*.jade', 'src/app/shared/**/*.jade'])
   .pipe(jade())
   .pipe(flatten())
-  .pipe(gulp.dest('./www/views'));
+  .pipe(gulp.dest('./www/views'))
+  .pipe(reload({stream:true}));
 });
 
 gulp.task('stylus', function(){  
   gulp.src('src/assets/stylesheets/*.styl')
   .pipe(stylus())
   .pipe(gulp.dest('www/assets/stylesheets'))
+  .pipe(reload({stream:true}));
 });
 
 gulp.task('move:libs', function() {
   gulp.src('./src/assets/libs/**/*', {base: './src/assets/libs/'})
-  .pipe(gulp.dest('./www/assets/libs/'));
+  .pipe(gulp.dest('./www/assets/libs/'))
 });
 
 gulp.task('move:images', function() {
   gulp.src('./src/assets/img/**/*', {base: './src/assets/img/'})
-  .pipe(gulp.dest('./www/assets/img/'));
+  .pipe(gulp.dest('./www/assets/img/'))
 });
 
 gulp.task('compile', ['concat', 'jade', 'stylus', 'move:libs', 'move:images']);
+
+gulp.task('serve', ['compile'], function () {
+
+  // Cria o server
+  browserSync.init({
+    server: {
+      baseDir: "./www"
+    }
+  });
+
+  gulp.watch(['src/app/**/*.js'],['concat'])
+  gulp.watch(['src/app/**/*.jade'],['jade'])
+  gulp.watch(['src/assets/stylesheets/**/*.styl'],['stylus'])
+  gulp.watch(['src/assets/libs/**/*'],['move:libs'])
+  gulp.watch(['src/assets/img/**/*'],['move:images'])
+
+});
